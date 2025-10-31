8c49aea447f7834d31d15e4c5d9105d163c5f31d
250166 <250166@newuu.uz>
2025-10-31 10:23:31 +0500

    9

--- main.cpp
+++ main.cpp
@@ -64,20 +64,29 @@ using namespace std;
 
 //problem 9
 // void reverseArray(int arr[], int start, int end) {
-//     for (int i = start; i <= end; i++) {
-//         swap(arr[i], arr[end]);
+//     int n = end;
+//     while (start < end) {
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
 //     }
-//     for (int i = 0; i < end - start; i++) {
+//     for (int i = 0; i <= n; i++) {
 //         cout << arr[i] << " ";
 //     }
 // }
 //
 // int main() {
-//     int arr[] = {};
-//     int input;
-//     while (cin >> input) {
-//         arr[input] = input;
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cout << "Enter element " << i + 1 << ": ";
+//         cin >> arr[i];
 //     }
+//     reverseArray(arr, 0, n - 1);
 // }
 
 //problem 10
